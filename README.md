c555cddbc91134a592ef284b68c00f1cf18f2770
imomushi8 <monnhannyarouze@gmail.com>
2026-03-29 17:40:32 +0900

    weztermのキーバインドに納得いってないけど、とりあえずめんどいからいったん終わりで

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# 初回に実行すること
+# Windowsのみ初回に実行すること
 ```powershell
 ### ※シンボリックリンクを作成するため、管理者権限もしくは開発者モードで開く必要あり。
 
 # クローンした位置を設定
-$env:DOTFILES = <クローンした位置>
+$DOTFILES=<クローンした位置>
+New-Item -ItemType SymbolicLink -Value $DOTFILES\PowerShell -Path $PROFILE\..\ | Out-Null
 
-# PowerShell
-New-Item -ItemType SymbolicLink -Value $env:DOTFILES\PowerShell -Path $PROFILE\../ | Out-Null
+# プロファイルの変更を反映
 . $PROFILE
+```
 
+# 必要なもののみ、以下でシンボリックリンクを作成
+```sh
 # Neovim
-ln -s $env:DOTFILES\nvim $env:XDG_CONFIG_HOME\nvim
+ln -s $DOTFILES/nvim $XDG_CONFIG_HOME/nvim
 
 # WezTerm
-ln -s $env:DOTFILES\wezterm $env:XDG_CONFIG_HOME\wezterm
+ln -s $DOTFILES/wezterm $XDG_CONFIG_HOME/wezterm
 ```
---
+++ wezterm/appearance.lua
@@ -0,0 +1,103 @@
+local M = {}
+
+-- ブランチ名:ディレクトリ名を記憶しておくテーブル
+local title_cache = {}
+
+----------------------------------------------------
+-- 外観の設定
+----------------------------------------------------
+function M.setup(wezterm, config)
+  ----------------------------------------------------
+  -- 全体
+  ----------------------------------------------------
+  config.window_background_opacity = 0.95
+  config.color_scheme = "Catppuccin Mocha"
+  config.font_size = 11.0
+  config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+  ----------------------------------------------------
+  -- ウィンドウ
+  ----------------------------------------------------
+  -- 各タブ（正確にはpane）に「ブランチ名:ディレクトリ名」を記憶させる
+  wezterm.on("update-status", function(window, pane)
+    local cwd_uri = wezterm.to_string(pane:get_current_working_dir())
+    local cwd = cwd_uri:gsub("\\", "/"):gsub("^file://", "")
+
+    if cwd then
+      local title = cwd
+
+      -- Gitのブランチ名を取得
+      local success, stdout, stderr = wezterm.run_child_process({
+        "git", "-C", cwd, "branch", "--show-current"
+      })
+
+      -- Gitブランチ名を取得できたら「ブランチ名:ディレクトリ名」と表示できるようにする
+      if success then
+        local branch = stdout:gsub("%s+", "")
+        title = branch .. ":" .. title
+      end
+
+      title_cache[pane:pane_id()] = title
+    end
+  end)
+
+  -- ウィンドウのタイトルを変更
+  wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+    local zoomed = ''
+    if tab.active_pane.is_zoomed then
+      zoomed = '[Z] '
+    end
+
+    local index = ''
+    if #tabs > 1 then
+      index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+    end
+
+    local title = tab.active_pane.title
+
+    local pane_id = pane.pane_id
+
+    -- 記憶させていた「ブランチ名:ディレクトリ名」を取り出す
+    if title_cache[pane_id] then
+      title = title_cache[pane_id]
+    end
+
+    return zoomed .. index .. title
+
+  end)
+
+  ----------------------------------------------------
+  -- タブ
+  ----------------------------------------------------
+  -- Windows Terminalっぽくするならこれ
+  -- config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
+  -- config.tab_max_width = 20
+  -- config.window_frame = {
+  --   font = wezterm.font { family = 'JetBrainsMono Nerd Font' },
+  --   font_size = 12.0, -- この値を大きくするとタブバーが太くなる
+  -- }
+
+  -- カスタマイズ性を重視するならこれ
+  config.use_fancy_tab_bar = false
+  config.tab_bar_at_bottom = true
+
+  -- タブのタイトルを変更
+  wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local original_title = tab.active_pane.title
+    local title = original_title:match("^.+\\(.+)$") or original_title
+
+    if string.find(original_title,"Administrator") ~= nil then
+      title = "🛡️" .. title
+    end
+
+    if #title > 20 then
+      title = string.sub(title, 1, 20) .. '..'
+    end
+    
+    return {
+      {Text = ' ' .. title .. ' '},
+    }
+  end)
+end
+
+return M
